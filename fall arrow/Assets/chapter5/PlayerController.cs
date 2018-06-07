3ba711345f7fabac8e90fc5f5bb9a7966fd7cef6
tkskd <g1727038@st.t-kougei.ac.jp>
2018-06-07 18:06:39 +0900

    fall_allow

---
+++ fall arrow/Assets/chapter5/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //左矢印を離したとき
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);//左に 3 動かす
+        }
+
+        //右矢印を離したとき
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);//右に 3 動かす
+        }
+	}
+}
